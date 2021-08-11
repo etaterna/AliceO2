241b155d03acb17f12ed3fe0cc879f533a488734
Monika Jakubowska <m.jakubowska@cern.ch>
2021-08-11 11:28:25 +0200

    Add CreateTableReal method, change input data

--- Analysis/Tutorials/src/pidProducer.cxx
+++ Analysis/Tutorials/src/pidProducer.cxx
@@ -7,9 +7,9 @@
 // In applying this license CERN does not waive the privileges and immunities
 // granted to it by virtue of its status as an Intergovernmental Organization
 // or submit itself to any jurisdiction.
-#include "Framework/runDataProcessing.h"
 #include "Framework/AnalysisTask.h"
 #include "Framework/AnalysisDataModel.h"
+#include "AnalysisDataModel/TrackSelectionTables.h"
 
 using namespace o2;
 using namespace o2::framework;
@@ -18,34 +18,69 @@ namespace o2::aod
 {
 namespace pidtracks
 {
-//DECLARE_SOA_COLUMN(EtaEmcal, etaemcal, float);
-//DECLARE_SOA_COLUMN(PhiEmcal, phiemcal, float);
-DECLARE_SOA_COLUMN(TPCSignal, tpcsignal, float);
-DECLARE_SOA_COLUMN(TOFSignal, tofsignal, float);
+////DECLARE_SOA_COLUMN(EtaEmcal, etaemcal, float);
+////DECLARE_SOA_COLUMN(PhiEmcal, phiemcal, float);
+//DECLARE_SOA_COLUMN(TPCSignal, tpcsignal, float);
+//DECLARE_SOA_COLUMN(TOFSignal, tofsignal, float);
 DECLARE_SOA_COLUMN(Px, px, float);
 DECLARE_SOA_COLUMN(Py, py, float);
 DECLARE_SOA_COLUMN(Pz, pz, float);
-DECLARE_SOA_COLUMN(PDGCode, pdgcode, float);
+DECLARE_SOA_COLUMN(Sign,sign,float);
+//DECLARE_SOA_COLUMN(X,x,float);
+//DECLARE_SOA_COLUMN(Y,y,float);
+//DECLARE_SOA_COLUMN(Z,z,float);
+//DECLARE_SOA_COLUMN(Alpha,alpha,float);
+//DECLARE_SOA_COLUMN(TrackType,tracktype,uint8_t);
+//DECLARE_SOA_COLUMN(TPCNClsShared, tpcNClsShared, uint8_t);
+//DECLARE_SOA_COLUMN(DcaXY, dcaXY, float);
+//DECLARE_SOA_COLUMN(DcaZ, dcaZ, float);
+//DECLARE_SOA_COLUMN(PdgCode, pdgCode, int);
 } // namespace pidtracks
-DECLARE_SOA_TABLE(PIDTracks, "AOD", "PIDTRACKS", pidtracks::EtaEmcal, pidtracks::PhiEmcal, pidtracks::TPCSignal, pidtracks::TOFSignal, pidtracks::P, pidtracks::PDGCode);
+DECLARE_SOA_TABLE(PIDTracksMC, "AOD", "PIDTRACKSMC", aod::track::TPCSignal, aod::track::TOFSignal, pidtracks::Px, pidtracks::Py, pidtracks::Pz, pidtracks::Sign, aod::track::X, aod::track::Y, aod::track::Z, aod::track::Alpha, aod::track::TrackType, aod::track::TPCNClsShared, aod::track::DcaXY, aod::track::DcaZ, aod::mcparticle::PdgCode);
+DECLARE_SOA_TABLE(PIDTracksReal, "AOD", "PIDTRACKSREAL", aod::track::TPCSignal, aod::track::TOFSignal, pidtracks::Px, pidtracks::Py, pidtracks::Pz, pidtracks::Sign, aod::track::X, aod::track::Y, aod::track::Z, aod::track::Alpha, aod::track::TrackType, aod::track::TPCNClsShared, aod::track::DcaXY, aod::track::DcaZ);
 } //namespace o2::aod
 
-struct CreateTrainingTable {
-  using BigTracksMC = soa::Join<aod::FullTracks, aod::McTrackLabels>;
+void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
+{
+  ConfigParamSpec optionDoMC{"doMC", VariantType::Bool, false, {"Fill PID train table with MC data."}};
+  workflowOptions.push_back(optionDoMC);
+}
 
-  Produces<aod::PIDTracks> pidTracksTable;
+#include "Framework/runDataProcessing.h"
+
+struct CreateTableMC {
+  using BigTracksMC = soa::Join<aod::FullTracks, aod::TracksExtended, aod::McTrackLabels>;
+
+  Produces<aod::PIDTracksMC> pidTracksTable;
   void process(BigTracksMC const& tracks, aod::McParticles const& mctracks)
   {
-
     for (const auto& track : tracks) {
       const auto mcParticle = track.mcParticle();
-      pidTracksTable(track.trackEtaEmcal(), track.trackPhiEmcal(), track.tpcSignal(), track.tofSignal(), track.px(), track.py(), track.pz(), mcParticle.pdgCode());
+      pidTracksTable(track.tpcSignal(), track.tofSignal(), track.px(), track.py(), track.pz(), track.sign(), track.x(), track.y(), track.z(), track.alpha(), track.trackType(), track.tpcNClsShared(), track.dcaXY(), track.dcaZ(), mcParticle.pdgCode());
+    }
+  }
+};
+
+struct CreateTableReal {
+  using BigTracks = soa::Join<aod::FullTracks, aod::TracksExtended>;
+
+  Produces<aod::PIDTracksReal> pidTracksTable;
+  void process(BigTracks const& tracks)
+  {
+    for (const auto& track : tracks) {
+      pidTracksTable(track.tpcSignal(), track.tofSignal(), track.px(), track.py(), track.pz(), track.sign(), track.x(), track.y(), track.z(), track.alpha(), track.trackType(), track.tpcNClsShared(), track.dcaXY(), track.dcaZ());
     }
   }
 };
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
-  return WorkflowSpec{
-    adaptAnalysisTask<CreateTrainingTable>(cfgc)};
+  const bool doMC = cfgc.options().get<bool>("doMC");
+  if (doMC) {
+    return WorkflowSpec{
+      adaptAnalysisTask<CreateTableMC>(cfgc)};
+  } else {
+    return WorkflowSpec{
+      adaptAnalysisTask<CreateTableReal>(cfgc)};
+  }
 }
